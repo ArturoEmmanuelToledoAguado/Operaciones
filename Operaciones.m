36c0da272b3603f58dede8e0985331d8d23cd402
ArturoEmmanuelToledoAguado <atoledoa1300@alumno.ipn.mx>
2022-09-14 12:08:17 -0500

    Sub menus

--- Operaciones.m
+++ Operaciones.m
@@ -4,8 +4,34 @@ close all;
 
 %Abrir imagen
 img=imread('IMG/j.jpg');
+
+%Variables de los menus
 opc=0;
+plano=0;
 
 while opc~=5
     opc=input('Seleccione una opcion\n1. Suma\n2. Resta\n3. Multiplicacion\n4. Division\n5. Salir\n')
+    if opc==1
+        opc=0;
+        while opc~4
+            opc=input('Selecione un plano\n1. Rojo (Red)\n2. Verde (Green)\n3. Azul (Blue).\n4. Salir\n')
+        end
+    elseif opc==2
+        opc=0;
+        while opc~4
+            opc=input('Selecione un plano\n1. Rojo (Red)\n2. Verde (Green)\n3. Azul (Blue).\n4. Salir\n')
+        end
+    elseif opc==3
+        opc=0;
+        while opc~4
+            opc=input('Selecione un plano\n1. Rojo (Red)\n2. Verde (Green)\n3. Azul (Blue).\n4. Salir\n')
+        end
+    elseif opc==4
+        opc=0;
+        while opc~4
+            opc=input('Selecione un plano\n1. Rojo (Red)\n2. Verde (Green)\n3. Azul (Blue).\n4. Salir\n')
+        end
+    else
+        break;
+    end
 end
